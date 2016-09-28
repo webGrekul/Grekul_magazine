2af34f716d1ff308cb57df4ad0643e9b572f3050
maxim <grekulmaxim@gmail.com>
2016-09-28 10:49:58 +0300

    grekul magazine

---
+++ app/js/common.js
@@ -0,0 +1,34 @@
+$(function() {
+$( ".btn-menu" ).click(function() {
+  $( ".panel-menu" ).show( "speed");
+});
+
+$('.close-menu').click(function() {
+  $('.panel-menu').hide( "speed");
+});
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
